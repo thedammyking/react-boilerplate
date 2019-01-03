835bf78581e795ae395c64adc23e416bc0fdad57
thedammyking <kingdammie@gmail.com>
2019-01-03 10:07:31 +0100

    change the store config

--- src/store.js
+++ src/store.js
@@ -4,10 +4,10 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import reducer from "./reducers";
 
-const getMiddleware = async () => {
+const getMiddleware = () => {
   let middleware = [thunk];
   if (process.env.NODE_ENV === "development") {
-    await middleware.push(logger);
+     middleware.push(logger);
     return composeWithDevTools(applyMiddleware(...middleware));
   }
   return applyMiddleware(...middleware);
