7cec1356879eabb3a32cee786814788b8491e30d
thedammyking <kingdammie@gmail.com>
2019-03-30 16:19:57 +0100

    update

--- src/router/AuthenticatedRoute.js
+++ src/router/AuthenticatedRoute.js
@@ -1,19 +1,21 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
 
-export default ({ component: C, props: cProps, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      cProps.isAuthenticated ? (
-        <C {...props} {...cProps} />
-      ) : (
-        <Redirect
-          to={`/login?redirect=${props.location.pathname}${
-            props.location.search
-          }`}
-        />
-      )
-    }
-  />
-);
+const AuthenticatedRoute = ({ component: C, props: cProps, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            cProps.isAuthenticated ? (
+                <C {...props} {...cProps} />
+            ) : (
+                <Redirect
+                    to={`/login?redirect=${props.location.pathname}${
+                        props.location.search
+                    }`}
+                />
+            )
+        }
+    />
+)
+
+export default AuthenticatedRoute
